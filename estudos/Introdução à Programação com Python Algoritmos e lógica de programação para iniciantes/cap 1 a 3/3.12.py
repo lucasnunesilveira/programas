a8ba8773eed729775a6cfdff16453e8cd50c451e
Lucas Nunes <42910365+lucasnunesilveira@users.noreply.github.com>
2020-02-20 12:56:05 -0300

    Add files via upload

---
+++ estudos/Introdução à Programação com Python Algoritmos e lógica de programação para iniciantes/cap 1 a 3/3.12.py
@@ -0,0 +1,5 @@
+import os 
+n1 = float(input('qual a distancia  pecorrido durante o trajeto  em KM : '))
+n2 = float(input(' qual velocidade media pecorrida durante o projeto em KM/H :  '))
+print (' o valor da da distancia pecorrida foi {:.2f}  , o valor da velocidade media foi {:.2f} , o valor do tempo foi {:.2f} hora '.format(n1 , n2 , n1/n2))
+os.system("pause")
